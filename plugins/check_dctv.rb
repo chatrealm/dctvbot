51e77e59132bc11efd50cf4d183a839556adcc69
Alex Hanna <tinnvec@gmail.com>
2015-08-25 16:48:55 -0400

    setup for test logging

--- plugins/check_dctv.rb
+++ plugins/check_dctv.rb
@@ -12,7 +12,22 @@ module Plugins
     end
 
     def listen(m)
+      channels = get_current_channels
+
+      channels.each do |channel|
+        output = "Ch. #{channel['channel']} - #{channel['friendlyalias']}"
+        output += " - Live" if is_live(channel)
+        output += " - Upcoming" if is_upcoming(channel)
+        @bot.log(output)
+      end
     end
-    
+
+    private
+
+      def get_current_channels
+        response = Net::HTTP.get_response(URI.parse('http://diamondclub.tv/api/channelsv2.php?v=3'))
+        return JSON.parse(response.body)['assignedchannels']
+      end
+
   end
 end
