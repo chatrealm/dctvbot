a0069221444d11e4b699bacaedc9cd37b4605c99
Alex Hanna <tinnvec@gmail.com>
2015-08-29 10:54:19 -0500

    Set currently active channels on restart

--- plugins/check_dctv.rb
+++ plugins/check_dctv.rb
@@ -9,6 +9,13 @@ module Plugins
 
     def initialize(*args)
       super
+      # Set announced arrays so as to not re-announce what's already on
+      @live_channels = Array.new
+      @soon_channels = Array.new
+      get_current_channels.each do |channel|
+        @live_channels << channel if is_live channel
+        @soon_channels << channel if is_upcoming channel
+      end
     end
 
     def listen(m)
@@ -27,6 +34,14 @@ module Plugins
       def get_current_channels
         response = Net::HTTP.get_response(URI.parse('http://diamondclub.tv/api/channelsv2.php?v=3'))
         return JSON.parse(response.body)['assignedchannels']
+      end
+
+      def is_live(channel)
+        return channel['nowonline'] == 'yes' && !channel['yt_upcoming']
+      end
+
+      def is_upcoming(channel)
+        return channel['nowonline'] == 'no' && channel['yt_upcoming']
       end
 
   end
