4ad90ab9bdc3af2a75744139bdd1cd79f5cfea0a
Alex Hanna <alexander.hanna@badgerlandfinancial.com>
2015-09-16 15:09:38 -0500

    Update DCTVBot to handle Ctrl-C and Kill

--- lib/dctvbot.rb
+++ lib/dctvbot.rb
@@ -1,7 +1,17 @@
-require 'cinch'
-
 class DCTVBot < Cinch::Bot
-  def initialize(*args)
+  def initialize(&b)
     super
+
+    # Handle SIGINT (Ctrl-C)
+    trap "SIGINT" do
+      debug "Caught SIGINT, quitting..."
+      self.quit
+    end
+
+    # Handle SIGTERM (Kill Command)
+    trap "SIGTERM" do
+      debug "Caught SIGTERM, quitting..."
+      self.quit
+    end
   end
 end
