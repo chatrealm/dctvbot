e5cf466fde12b6835c3688b93c675ac84330b82b
Alex Hanna <tinnvec@gmail.com>
2015-09-17 11:00:53 -0400

    Remove extraneous tests

--- spec/dctvbot_spec.rb
+++ spec/dctvbot_spec.rb
@@ -17,19 +17,13 @@ describe DCTVBot do
   end
 
   describe "attributes" do
-    it { is_expected.to respond_to(:callback) }
-    it { is_expected.to respond_to(:channel_list) }
     it { is_expected.to respond_to(:channels) }
     it { is_expected.to respond_to(:config) }
-    it { is_expected.to respond_to(:handlers) }
-    it { is_expected.to respond_to(:irc) }
-    it { is_expected.to respond_to(:last_connection_was_successful) }
-    it { is_expected.to respond_to(:loggers) }
-    it { is_expected.to respond_to(:modes) }
     it { is_expected.to respond_to(:nick) }
-    it { is_expected.to respond_to(:plugins) }
-    it { is_expected.to respond_to(:quitting) }
-    it { is_expected.to respond_to(:user_list) }
   end
+
+  # describe "#custom_log_file" do
+  #   # Figure out how to test this
+  # end
 
 end
