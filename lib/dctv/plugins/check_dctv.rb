61615f5763b50e650a1a195603f773c2f0f3b92b
Alex Hanna <tinnvec@gmail.com>
2015-09-17 22:11:07 -0500

    Move remaining methods to check_dcv handler and comment

--- lib/dctv/plugins/check_dctv.rb
+++ lib/dctv/plugins/check_dctv.rb
@@ -2,24 +2,26 @@ module DCTV
   module Plugins
 
     class CheckDCTV
-      # Include Cinch stuff
+      # Cinch plugin
       include Cinch::Plugin
 
-      # Already announced channels
-      attr_accessor :already_announced
-
-      # Event the plugin responds to
+      # Handler to respond to
       listen_to :check_dctv
 
       def initialize(*args)
         super
 
-        @bot.update_dctv_status
-
+        # Already announced channels
         @already_announced = Array.new
-        @bot.assignedchannels.each do |stream|
-          @already_announced << stream
-        end
+
+        # Official channel live status
+        @official_channel_live = false
+
+        # Mark currently live channels as already announced
+        update_dctv_status
+        # @bot.assignedchannels.each do |channel|
+        #   @already_announced << channel
+        # end
 
         # @bot.assignedchannels = JSON.parse( '[
         #   {
@@ -43,34 +45,114 @@ module DCTV
 
       # Executed on event trigger
       def listen(m)
-        # Update assigned channels on dctv
-        @bot.update_dctv_status
-
-        # Remove formerly upcoming channels from announced channels list if no longer upcoming
-        announced_upcoming = @already_announced.find_all { |aa| aa['yt_upcoming'] }
-        announced_upcoming.each do |au|
-          stream = @bot.assignedchannels.find { |st| st['streamid'] == au['streamid'] }
-          @already_announced.delete(stream) unless stream.nil? || stream['yt_upcoming']
+        # Update assigned channels and official live status from dctv
+        update_dctv_status
+        # Clean already announced list of upcoming and live channels when their status has changed
+        clean_announced_list
+        # Check all currently assigned channels
+        @bot.assignedchannels.each do |channel|
+          # Skip announcements if official cahnnel is live
+          next if @official_channel_live || @already_announced.include?(channel)
+          # Announce/Update topic
+          announce_channel(channel)
+          # Add channel to list of already announced channels
+          @already_announced << channel
         end
 
-        # Remove offline channels from announced channels list
-        @already_announced.each do |aa|
-          unless @bot.assignedchannels.any? { |ac| ac['streamid'] == aa['streamid'] }
-            @already_announced.delete(aa)
+        @bot.debug "check_dctv handler complete."
+      end
+
+      private
+
+        # Checks if channel is official
+        def is_official(channel)
+          channel['channel'] == 1
+        end
+
+        # Checks if channel is online
+        def is_online(channel)
+          channel['nowonline'] == "yes"
+        end
+
+        # Updates assignedchannels from dctv api
+        def update_assignedchannels
+          response = Net::HTTP.get_response(URI.parse('http://diamondclub.tv/api/channelsv2.php?v=3'))
+          @bot.assignedchannels = JSON.parse(response.body)['assignedchannels']
+        end
+
+        # Refreshes official channel live status
+        def update_official_live
+          @official_live = false
+          @bot.assignedchannels.each do |channel|
+            @official_live = true if is_online(channel) && is_official(channel)
           end
         end
 
-        @bot.assignedchannels.each do |stream|
-          # Skip announcements if official cahnnel is live
-          next if @bot.official_live || @already_announced.include?(stream)
-          # Announce/Update topic
-          @bot.announce_stream(stream)
-          # Add channel to list of already announced channels
-          @already_announced << stream
+        # Triggers update methods for dctv status
+        def update_dctv_status
+          update_assignedchannels
+          update_official_live
         end
 
-        @bot.debug "check_dctv event complete."
-      end
+        # Formats channel announcement message
+        def get_announce_message(channel)
+          # Extract the info we need crom channel object
+          name        = channel['friendlyalias']
+          description = channel['twitch_yt_description']
+          url         = channel['urltoplayer']
+          upcoming    = channel['yt_upcoming']
+          # Set color formatted status
+          status = Format(:white, :red, " LIVE ")
+          status = Format(:black, :yellow, " UP NEXT ") if upcoming
+          # Piece together message from available info
+          msg  = "#{status} #{name}"
+          msg += " - #{description}" unless description.empty?
+          msg += " - #{url}" unless url.empty?
+        end
+
+        # Does channel live/upcoming announcement
+        def announce_channel(channel)
+          # Set announce message
+          output = get_announce_message(channel)
+          # Announce channel
+          @bot.primary_channel.send(output)
+          # Update topic, if channel is official
+          update_channel_topic(output) if is_official(channel)
+        end
+
+        # Updates primary channel's topic from given title, preserving existing title after first "|"
+        # Example: 'first | second | third' -> update_primary_channel_topic('apple') -> 'apple | second | third'
+        def update_primary_channel_topic(title)
+          # Get array of items between "|" characters
+          topic_array = @bot.primary_channel.topic.split("|")
+          # Remove first item
+          topic_array.shift
+          # Add new info to beginning of former title
+          new_topic = title + " |" + topic_array.join("|")
+          # Set primary channel topic
+          @bot.primary_channel.topic = new_topic
+        end
+
+        def clean_announced_list
+          # Remove formerly upcoming channels from announced channels list if no longer upcoming
+          announced_upcoming = @already_announced.find_all { |aa| aa['yt_upcoming'] }
+          announced_upcoming.each do |au|
+            channel = @bot.assignedchannels.find { |ch| ch['streamid'] == au['streamid'] }
+            unless channel.nil? || channel['yt_upcoming']
+              @already_announced.delete(channel)
+            end
+          end
+
+          # Remove offline channels from announced channels list
+          @already_announced.each do |aa|
+            @already_announced.delete(aa)
+            @bot.assignedchannels.each do |ac|
+              if aa['streamid'] == ac['streamid']
+                @already_announced << ac
+              end
+            end
+          end
+        end
     end
 
   end
--- lib/dctvbot.rb
+++ lib/dctvbot.rb
@@ -3,11 +3,8 @@ require 'json'
 require 'net/http'
 
 class DCTVBot < Cinch::Bot
-  # Assigned channels
+  # Assigned channels (from dctv api)
   attr_accessor :assignedchannels
-
-  # Official channel live boolean
-  attr_accessor :official_live
 
   # Command Control
   # attr_accessor :cleverbot_enabled, :dctv_commands_enabled
@@ -17,8 +14,6 @@ class DCTVBot < Cinch::Bot
 
   def initialize(&block)
     super
-
-    update_assignedchannels
 
     # Handle SIGINT (Ctrl-C)
     trap "SIGINT" do
@@ -38,60 +33,10 @@ class DCTVBot < Cinch::Bot
     file = open(file_name, 'a')
     file.sync = true # Write buffered data immediately
     @loggers << Cinch::Logger::FormattedLogger.new(file)
-    @loggers.first.level = log_level
+    @loggers.first.level = log_level # Set log level
   end
 
   def primary_channel
     Channel(@channels.first)
   end
-
-  def update_dctv_status
-    update_assignedchannels
-    update_official_live
-  end
-
-  def announce_stream(stream)
-    name = stream['friendlyalias']
-    url = stream['urltoplayer']
-    description = stream['twitch_yt_description']
-    upcoming = stream['yt_upcoming']
-
-    # Set announce message
-    output = get_announce_message(name, url, description, upcoming)
-    # Announce channel
-    primary_channel.send(output)
-    # Update topic, if channel is official
-    update_channel_topic(output) if stream['channel'] == 1
-  end
-
-  def get_announce_message(name, url, description=nil, upcoming=false)
-    status = Format(:white, :red, " LIVE ")
-    status = Format(:black, :yellow, " UP NEXT ") if upcoming
-    msg = "#{status} #{name}"
-    msg += " - #{description}" unless description.empty?
-    msg += " - #{url}"
-  end
-
-  def update_channel_topic(title, irc_channel=nil)
-    irc_channel = primary_channel if irc_channel.nil?
-    topic_array = irc_channel.topic.split("|")
-    topic_array.shift
-    new_topic = title + " |" + topic_array.join("|")
-    irc_channel.topic = new_topic
-  end
-
-  private
-
-    def update_assignedchannels
-      response = Net::HTTP.get_response(URI.parse('http://diamondclub.tv/api/channelsv2.php?v=3'))
-      @assignedchannels = JSON.parse(response.body)['assignedchannels']
-    end
-
-    def update_official_live
-      @official_live = false
-      @assignedchannels.each do |stream|
-        @official_live = true if stream['nowonline'] == "yes" && stream['channel'] == 1
-      end
-    end
-
 end
