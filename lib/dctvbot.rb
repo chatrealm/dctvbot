b9e14b864291956c69f128703f9e18c41b22afb3
Alex Hanna <tinnvec@gmail.com>
2015-09-18 21:01:44 -0500

    Move all initial configuration into initialize function

--- lib/dctvbot.rb
+++ lib/dctvbot.rb
@@ -1,19 +1,68 @@
 require 'cinch'
+require 'cinch/extensions/authentication'
+require 'cinch/plugins/identify'
 require 'json'
 require 'net/http'
+
+require_relative 'cinch/plugins/help'
+require_relative 'cinch/plugins/plugin_management'
+
+require_relative 'dctv/plugins/channel_status'
+require_relative 'dctv/plugins/check_dctv'
 
 class DCTVBot < Cinch::Bot
   # Assigned channels (from dctv api)
   attr_accessor :assignedchannels
-
-  # Command Control
-  # attr_accessor :cleverbot_enabled, :dctv_commands_enabled
-
   # Twitter Endpoint
-  # attr_accessor :twitter
+  attr_accessor :twitter
 
-  def initialize(&block)
-    super
+  def initialize(config_file)
+    super()
+
+    # Define Cinch Configuration
+    configure do |c|
+      # Server Info
+      c.server  = config_file['server']['host']
+      c.port    = config_file['server']['port']
+
+      # Bot User Info
+      c.nick      = config_file['bot']['nick']
+      c.user      = config_file['bot']['user']
+      c.realname  = config_file['bot']['realname']
+      c.channels  = config_file['bot']['channels']
+
+      # Authentication Plugin Settings
+      c.authentication          = Cinch::Configuration::Authentication.new
+      c.authentication.strategy = :channel_status
+      c.authentication.level    = config_file['authentication']['minimum-level']
+
+      # Load Up Plugins
+      c.plugins.plugins = [
+        Cinch::Plugins::Help,
+        Cinch::Plugins::Identify,
+        Cinch::Plugins::PluginManagement,
+        DCTV::Plugins::ChannelStatus,
+        DCTV::Plugins::CheckDCTV
+      ]
+
+      # Set Plugin Options
+      c.plugins.options = {
+        Cinch::Plugins::Identify => { type: :nickserv, password: config_file['bot']['password'] },
+        Cinch::Plugins::PluginManagement => { authentication_level: config_file['authentication']['admin-level'] }
+      }
+    end
+
+    # Setup Twitter Endpoint
+    # twitter = Twitter::REST::Client.new do |c|
+    #   c.consumer_key        = config_file['plugins']['twitter']['consumer-key']
+    #   c.consumer_secret     = config_file['plugins']['twitter']['consumer-secret']
+    #   c.access_token        = config_file['plugins']['twitter']['access-token']
+    #   c.access_token_secret = config_file['plugins']['twitter']['access-token-secret']
+    # end
+
+    # Set custom Log File
+    # TODO: Uncomment this before production
+    # custom_log_file(config_file['custom-log']['file'], config_file['custom-log']['level'])
 
     # Handle SIGINT (Ctrl-C)
     trap "SIGINT" do
