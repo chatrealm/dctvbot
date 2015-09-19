4a7136c45540261e6977b259e7df710005dbc020
Alex Hanna <tinnvec@gmail.com>
2015-09-18 21:02:36 -0500

    Fully working CheckDCTV handler

--- lib/dctv/plugins/check_dctv.rb
+++ lib/dctv/plugins/check_dctv.rb
@@ -7,19 +7,20 @@ module DCTV
       # Set plugin name, help text and options
       set(
         plugin_name: 'CheckDCTV',
-        help: 'Updates diamondclub.tv information and announces live/upcoming shows.'
+        help: 'Announces live/upcoming shows.'
       )
       # Handler to respond to
       listen_to :check_dctv
 
       def initialize(*args)
         super
+        @@i = 0
         # Already announced channels
         @already_announced = Array.new
         # Official channel live status
-        @official_channel_live = false
+        @official_live = false
         # Mark currently live channels as already announced
-        update_dctv_status
+        update_assignedchannels
         @bot.assignedchannels.each do |channel|
           @already_announced << channel
         end
@@ -27,25 +28,33 @@ module DCTV
 
       # Executed on event trigger
       def listen(m)
-        # Update assigned channels and official live status from dctv
-        update_dctv_status
-        # Clean already announced list of upcoming and live channels when their status has changed
-        clean_announced_list
+        # Update assigned channels from dctv
+        update_assignedchannels#_debug
+        @bot.debug "Assigned Channels from DCTV api:"
+        @bot.assignedchannels.each do |channel|
+          @bot.debug "#{channel['channel']}. #{channel['friendlyalias']} - #{is_online?(channel) ? 'Live' : 'Upcoming'}"
+        end
 
-        @bot.debug "Announced Channels:"
+        # Clean already announced list of upcoming and live channels when their status has changed
+        refresh_announced_list
+        @bot.debug "Already Live Channels:"
         @already_announced.each do |channel|
-          @bot.debug "#{channel['channel']}. #{channel['friendlyalias']} - #{channel['online'] == "yes" ? 'Live' : 'Upcoming'}"
+          @bot.debug "#{channel['channel']}. #{channel['friendlyalias']} - #{is_online?(channel) ? 'Live' : 'Upcoming'}"
         end
 
         # Check all currently assigned channels
         @bot.assignedchannels.each do |channel|
           # Skip announcements if official cahnnel is live
-          next if @official_channel_live || @already_announced.include?(channel)
+          next if @official_live || @already_announced.include?(channel)
           # Announce/Update topic
           announce_channel(channel)
           # Add channel to list of already announced channels
           @already_announced << channel
         end
+
+        # Update official live status from dctv
+        update_official_live
+        @bot.debug "Official Live? #{@official_live}"
 
         @bot.debug "check_dctv handler complete."
       end
@@ -53,13 +62,18 @@ module DCTV
       private
 
         # Checks if channel is official
-        def is_official(channel)
+        def is_official?(channel)
           channel['channel'] == 1
         end
 
         # Checks if channel is online
-        def is_online(channel)
+        def is_online?(channel)
           channel['nowonline'] == "yes"
+        end
+
+        # Checks if channel is upcoming
+        def is_upcoming?(channel)
+          channel['yt_upcoming']
         end
 
         # Updates assignedchannels from dctv api
@@ -68,20 +82,61 @@ module DCTV
           @bot.assignedchannels = JSON.parse(response.body)['assignedchannels']
         end
 
+        # Updates assignedchannels from dctv api
+        def update_assignedchannels_debug
+          case @@i
+          when 2..3
+            @bot.assignedchannels = JSON.parse('[
+              {
+                "streamid": 300,
+                "channelname": "DailyTechNewsShow",
+                "friendlyalias": "DTNS",
+                "streamtype": "youtube",
+                "nowonline": "no",
+                "alerts": true,
+                "twitch_currentgame": "",
+                "twitch_yt_description": "DTNS 2584 - with Erin Carson",
+                "yt_upcoming": true,
+                "yt_liveurl": "https://www.youtube.com/watch?v=ZjNjv959uUI",
+                "imageasset": "http://diamondclub.tv/i/5f5c5ebe764eaf99143188b75a4e021a2b121883.png",
+                "imageassethd": "",
+                "urltoplayer": "http://dctv.link/1",
+                "channel": 1
+              }
+            ]')
+          when 4..5
+            @bot.assignedchannels = JSON.parse('[
+              {
+                "streamid": 300,
+                "channelname": "DailyTechNewsShow",
+                "friendlyalias": "DTNS",
+                "streamtype": "youtube",
+                "nowonline": "yes",
+                "alerts": true,
+                "twitch_currentgame": "",
+                "twitch_yt_description": "DTNS 2584 - with Erin Carson",
+                "yt_upcoming": false,
+                "yt_liveurl": "https://www.youtube.com/watch?v=ZjNjv959uUI",
+                "imageasset": "http://diamondclub.tv/i/5f5c5ebe764eaf99143188b75a4e021a2b121883.png",
+                "imageassethd": "",
+                "urltoplayer": "http://dctv.link/1",
+                "channel": 1
+              }
+            ]')
+          else
+            update_assignedchannels
+          end
+          @@i += 1
+        end
+
         # Refreshes official channel live status
         def update_official_live
           was_official_live = @official_live
           @official_live = false
           @bot.assignedchannels.each do |channel|
-            @official_live = true if is_online(channel) && is_official(channel)
+            @official_live = true if is_online?(channel) && is_official?(channel)
           end
           update_primary_channel_topic(' <>') if was_official_live && !@official_live
-        end
-
-        # Triggers update methods for dctv status
-        def update_dctv_status
-          update_assignedchannels
-          update_official_live
         end
 
         # Formats channel announcement message
@@ -90,7 +145,7 @@ module DCTV
           name        = channel['friendlyalias']
           description = channel['twitch_yt_description']
           url         = channel['urltoplayer']
-          upcoming    = channel['yt_upcoming']
+          upcoming    = is_upcoming?(channel)
           # Set color formatted status
           status = Format(:white, :red, " LIVE ")
           status = Format(:black, :yellow, " UP NEXT ") if upcoming
@@ -107,7 +162,7 @@ module DCTV
           # Announce channel
           @bot.primary_channel.send(output)
           # Update topic, if channel is official
-          update_primary_channel_topic(output) if is_official(channel)
+          update_primary_channel_topic(output) if is_official?(channel)
         end
 
         # Updates primary channel's topic from given title, preserving existing title after first "|"
@@ -123,32 +178,21 @@ module DCTV
           @bot.primary_channel.topic = new_topic
         end
 
-        def clean_announced_list
-          remove_no_longer_upcoming_channels
-          remove_offline_channels
+        def refresh_announced_list
+          clean_announced_list(@already_announced.find_all { |announced| is_upcoming?(announced) }, true)
+          clean_announced_list(@already_announced.find_all { |announced| !is_upcoming?(announced) })
         end
 
-        # Remove formerly upcoming channels from announced channels list if no longer upcoming
-        def remove_no_longer_upcoming_channels
-          announced_upcoming = @already_announced.find_all { |aa| aa['yt_upcoming'] }
-          announced_upcoming.each do |au|
-            channel = @bot.assignedchannels.find { |ch| ch['streamid'] == au['streamid'] }
-            unless channel.nil? || channel['yt_upcoming']
-              @already_announced.delete(channel)
-              @bot.debug "Removing #{channel['friendlyalias']} from already announced - no longer upcoming"
-            end
-          end
-        end
-
-        # Remove offline channels from announced channels list
-        def remove_offline_channels
-          @already_announced.each do |aa|
-            channel = @bot.assignedchannels.find { |ac| aa['streamid'] == ac['streamid'] }
-            @already_announced.delete(aa)
-            if !channel.nil? && (is_online(channel) || channel['yt_upcoming'])
-              @already_announced << channel
+        def clean_announced_list(channel_array, upcoming=false)
+          channel_array.each do |existing_ch|
+            @bot.debug "Checking to see if #{existing_ch['friendlyalias']} is still there"
+            channel = @bot.assignedchannels.find { |assigned| existing_ch['streamid'] == assigned['streamid'] }
+            @already_announced.delete(existing_ch)
+            if channel.nil? || (upcoming && !is_upcoming?(channel)) || (!upcoming && !is_online?(channel))
+              @bot.debug "Nope, removing from already announced"
             else
-              @bot.debug "Removing #{channel['friendlyalias']} from already announced - no longer live"
+              @bot.debug "Yep, updating details"
+              @already_announced << channel
             end
           end
         end
