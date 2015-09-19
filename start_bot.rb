f1e1c8558b875d0dd885e50c61cf554a91bab00e
Alex Hanna <tinnvec@gmail.com>
2015-09-19 10:55:24 -0500

    Add log folder

--- start_bot.rb
+++ start_bot.rb
@@ -4,7 +4,7 @@ require_relative 'lib/dctvbot'
 require_relative 'lib/watcher'
 
 # Instance of dctvbot with yaml config file
-dctvbot = DCTVBot.new(YAML.load(File.open 'config.test.yml'))
+dctvbot = DCTVBot.new(YAML.load(File.open 'config.yml'))
 # Start watcher threads
 Thread.new { Watcher.new(dctvbot, :check_dctv).start }
 Thread.new { Watcher.new(dctvbot, :check_twitter, 300).start }
