9b28e4bd698e40cd4b805cdf1c6c6af22243e7dc
Alex Hanna <tinnvec@gmail.com>
2015-09-19 13:21:35 -0500

    Fix initialize function that didn't make the move

--- lib/cinch/plugins/clever_bot.rb
+++ lib/cinch/plugins/clever_bot.rb
@@ -8,6 +8,11 @@ module Cinch
 
 			match lambda { |m| /(.*)\s?@?#{m.bot.nick}[:,]?\s*(.*)/i }, use_prefix: false
 
+			def initialize(*args)
+				super
+				@cleverbot = Cleverbot.new
+			end
+
 			def execute(m, part_one, part_two=nil)
 				response = @cleverbot.write "#{part_one} #{part_two}"
 				m.reply response, true
