7e86bc0ae580a8006ab834a2d7ab151620bbadbf
Alex Hanna <tinnvec@gmail.com>
2016-04-17 11:57:51 -0500

    reset to blank slate

--- lib/dctvbot.rb
+++ lib/dctvbot.rb
@@ -2,21 +2,15 @@
 
 class Dctvbot
     # public properties
-    attr_accessor :nick
 
     # public methods
 
     def initialize
-        @nick = '';
     end
 
-    def set_option(property, value)
-        # raise an error if the property doesn't exist
-        raise "Property does not exist." unless instance_variable_defined? "@#{property}"
+    # private methods
 
-        # set supplied property to supplied value
-        instance_variable_set("@#{property}", value)
-    end
+    private
 
     # set options/config
     # Connect to twitter, irc, and discord (+ slack?)
--- test/dctvbot_test.rb
+++ test/dctvbot_test.rb
@@ -1,22 +1,8 @@
 # file: test/dctvbot_test.rb
 
 require 'test_helper'
-
 require 'dctvbot'
 
 class DctvbotTest < Minitest::Test
-
-    def test_set_option_sets_property
-        dctvbot = Dctvbot.new
-        dctvbot.set_option(:nick, 'dctvbot')
-        assert_equal(dctvbot.nick, 'dctvbot', "Expected 'dctvbot', got #{dctvbot.nick}")
-    end
-
-    def test_set_option_raises_error_with_nonexistent_property
-        assert_raises "Property does not exist." do
-            dctvbot = Dctvbot.new
-            dctvbot.set_option(:notaproperty, 'some value')
-        end
-    end
 
 end
