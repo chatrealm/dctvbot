55f395dbcb371927197db3b98155af08e848aa4a
Alex Hanna <tinnvec@gmail.com>
2016-04-17 07:12:55 -0500

    enact better naming convention

---
+++ test/dctvbot_test.rb
@@ -0,0 +1,7 @@
+# file: test/dctvbot_test.rb
+
+require 'minitest/autorun'
+
+class DctvbotTest < Minitest::Test
+    # TODO: Write tests
+end
--- test/test_dctvbot.rb
+++
@@ -1,7 +0,0 @@
-# file: test/test_dctvbot.rb
-
-require 'minitest/autorun'
-
-class TestDctvbot < Minitest::Test
-    # TODO: Write tests
-end
