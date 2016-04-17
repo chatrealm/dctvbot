1b91a735e3271f3ecd15ac4a097a2f907d7788da
Alex Hanna <tinnvec@gmail.com>
2016-04-17 13:04:50 -0500

    add setup method for tests

--- test/dctvbot_test.rb
+++ test/dctvbot_test.rb
@@ -4,6 +4,9 @@ require 'test_helper'
 require 'dctvbot'
 
 class DctvbotTest < Minitest::Test
+    def setup
+        @dctvbot = Dctvbot.new('config.test.yml')
+    end
 
     def test_initialize_loads_config_from_supplied_yaml_file
         dctvbot = Dctvbot.new('config.test.yml')
