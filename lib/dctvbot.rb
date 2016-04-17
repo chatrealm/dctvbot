a169fcb45e37abe85db2aa5b1f2edf789c4a31f1
Alex Hanna <tinnvec@gmail.com>
2016-04-17 17:08:21 -0500

    cleanup dctvbot file

--- lib/dctvbot.rb
+++ lib/dctvbot.rb
@@ -3,19 +3,19 @@
 require 'yaml'
 
 class Dctvbot
-    # public properties
 
     # read-only properties
     attr_reader :config
 
     # public methods
-
     def initialize(config_file)
+
+        # load config from yaml
         @config = YAML.load(File.open config_file)
+        
     end
 
     # private methods
-
     private
 
     # set options/config
