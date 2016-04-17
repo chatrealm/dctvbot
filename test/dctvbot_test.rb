2484884ec7f6f4ffe90e9e6d4ca1fe5b9eda469d
Alex Hanna <tinnvec@gmail.com>
2016-04-17 11:40:27 -0500

    add test for set_option when specified property doesn't exist

--- test/dctvbot_test.rb
+++ test/dctvbot_test.rb
@@ -9,7 +9,14 @@ class DctvbotTest < Minitest::Test
     def test_set_option_sets_property
         dctvbot = Dctvbot.new
         dctvbot.set_option(:nick, 'dctvbot')
-        assert_equal dctvbot.nick, 'dctvbot', "Expected 'dctvbot', got #{dctvbot.nick}"
+        assert_equal(dctvbot.nick, 'dctvbot', "Expected 'dctvbot', got #{dctvbot.nick}")
+    end
+
+    def test_set_option_raises_error_with_nonexistent_property
+        assert_raises "Property does not exist." do
+            dctvbot = Dctvbot.new
+            dctvbot.set_option(:notaproperty, 'some value')
+        end
     end
 
 end
