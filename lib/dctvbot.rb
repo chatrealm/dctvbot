4452eb16c8eac16b239e13b428f9f48fe359a500
Alex Hanna <tinnvec@gmail.com>
2016-04-17 10:54:32 -0500

    add set_option method for setting options on dctvbot

---
+++ lib/dctvbot.rb
@@ -0,0 +1,13 @@
+# file: lib/dctvbot.rb
+
+class Dctvbot
+
+    def set_option(property, value)
+        property = value
+    end
+
+    # set options/config
+    # Connect to twitter, irc, and discord (+ slack?)
+    # listen for commands, mentions, or other important conversation
+    # respond to commands as nessecary
+end
