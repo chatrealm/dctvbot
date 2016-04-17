0037c8bc55ee4c86c889556024edc5aa1ba3a316
Alex Hanna <tinnvec@gmail.com>
2016-04-17 14:47:53 -0500

    add setup method for twitter_service tests

--- test/twitter_service_test.rb
+++ test/twitter_service_test.rb
@@ -1,6 +1,15 @@
 # file: test/twitter_service_test.rb
 
+require 'yaml'
 require 'test_helper'
 
 class TwitterServiceTest < Minitest::Test
+
+    # setup method
+    def setup
+
+        # load config file
+        @config = YAML.load(File.open 'config.test.yml')
+
+    end
 end
