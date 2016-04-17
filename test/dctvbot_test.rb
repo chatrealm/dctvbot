89c0a2994935dec9a42d7af4bf22cbdcf7f2f968
Alex Hanna <tinnvec@gmail.com>
2016-04-17 11:12:48 -0500

    better assertion with custom error message for test_sets_property

--- test/dctvbot_test.rb
+++ test/dctvbot_test.rb
@@ -9,7 +9,7 @@ class DctvbotTest < Minitest::Test
     def test_set_option_sets_property
         dctvbot = Dctvbot.new
         dctvbot.set_option(:nick, 'dctvbot')
-        assert dctvbot.nick == 'dctvbot'
+        assert_equal dctvbot.nick, 'dctvbot', "Expected 'dctvbot', got #{dctvbot.nick}"
     end
 
 end
