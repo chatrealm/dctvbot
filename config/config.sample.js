ca6b8d07d9767a4d7766107cd863402536ca0b5a
Alex Hanna <tinnvec@gmail.com>
2016-08-06 10:17:35 -0500

    Text alignment

--- config/config.sample.js
+++ config/config.sample.js
@@ -2,14 +2,14 @@ var config = module.exports = {};
 
 // Bot config
 config.bot = {
-	nick: 'nick',
-	userName: 'user name',
-	realName: 'real name'
+	nick:		'nick',
+	userName:	'user name',
+	realName:	'real name'
 };
 
 // Server config
 config.server = {
-	address: 'host.address',
-	port: 6667,
-	channels: [ '#channel' ]
+	address:	'host.address',
+	port:		6667,
+	channels:	[ '#channel' ]
 };
