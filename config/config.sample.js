b37956eb4daca0edb702a961aaeda02a78d933f3
Alex Hanna <tinnvec@gmail.com>
2016-08-20 08:00:42 -0500

    update sample config

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,19 +1,26 @@
 export default {
+
+    // Bot config
     bot: {
         nick: 'nick',
         userName: 'user name',
         realName: 'real name'
     },
 
+    // Server config
     server: {
         address: 'host.address',
         port: 6667,
         channels: ['#channel']
     },
 
+    // Commands config
     prefix: '!',
 
+    // Google Calendar config
     google: {
-        apiKey: ''
+        apiKey: '',
+        calendarId: ''
     }
+
 };
